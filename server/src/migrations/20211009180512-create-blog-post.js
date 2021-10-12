252cd6f8f011f00f120835811a89b86e49b0c44d
Antenor <antenorzapata@gmail.com>
2021-10-12 10:44:42 -0300

    refactor: update migration and increase the number of characters in the content string

--- server/src/migrations/20211009180512-create-blog-post.js
+++ server/src/migrations/20211009180512-create-blog-post.js
@@ -15,7 +15,7 @@ module.exports = {
       },
       content: {
         allowNull: false,
-        type: Sequelize.STRING,
+        type: Sequelize.STRING(2600),
       },
       userId: {
         allowNull: false,
